e59d51ddf412de5efdf8324ab05143459180af0d
shaikhamaan <amaans@sahaj.ai>
2023-02-21 20:54:21 +0530

    Add order history and place order css

--- js/order/esopDiv.js
+++ js/order/esopDiv.js
@@ -5,8 +5,8 @@ const updateEsopDiv = () => {
   let newELement = ` 
     <label for="order-esop-type">Esop Type</label>
     <select name="order-esop-type" id="order-esop-type">
-        <option value="NON_PERFORMANCE">NON_PERFORMANCE</option>
-        <option value="PERFORMANCE">PERFORMANCE</option>
+        <option value="NON_PERFORMANCE">Non Performance</option>
+        <option value="PERFORMANCE">Performance</option>
     </select>
     `;
 
